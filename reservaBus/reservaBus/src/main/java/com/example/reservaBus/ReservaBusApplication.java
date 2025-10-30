9a41bd227005599c898444be05e8eb74fc9db411
Luis Rincón <luiferrincon21@gmail.com>
2025-10-29 22:52:42 -0500

    Agregado proyecto Spring Boot reservaBus

---
+++ reservaBus/reservaBus/src/main/java/com/example/reservaBus/ReservaBusApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservaBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaBusApplication.class, args);
+	}
+
+}
---
+++ reservaBus/reservaBus/src/test/java/com/example/reservaBus/ReservaBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservaBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
