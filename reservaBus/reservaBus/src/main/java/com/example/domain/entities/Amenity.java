529b697a938cb2a31c4f80551abb9a2c7e7422bc
Jean Vides <videsrudas@proton.me>
2025-11-05 22:54:39 -0500

    add: amenities json

--- reservaBus/reservaBus/src/main/java/com/example/domain/entities/Amenity.java
+++ reservaBus/reservaBus/src/main/java/com/example/domain/entities/Amenity.java
@@ -16,7 +16,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,39 +23,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "buses")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
-public class Bus {
+public class Amenity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
-    private String plate;
+    private String name;
 
     @Column(nullable = false)
-    private Integer capacity;
-
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Builder.Default
-    private List<Amenity> amenities = new ArrayList<>();
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private BusStatus status;
-
-    @OneToMany(mappedBy = "bus")
-    @Builder.Default
-    private List<Seat> seats = new ArrayList<>();
-
-    @OneToMany(mappedBy = "bus")
-    @Builder.Default
-    private List<Trip> trips = new ArrayList<>();
+    private String description;
 
 }
